4c7c4f4df11caa4438b837fc291c27286a44efb6
scottlefoll <51521131+scottlefoll@users.noreply.github.com>
2022-06-23 08:55:35 -0400

    update

---
+++ W03 Prove - Jumper/main.py
@@ -0,0 +1,36 @@
+"""Jumper Specification
+There are old skydivers and bold skydivers,
+but there are no old, bold skydivers.
+
+- Jeff Wuorio -
+Overview
+Jumper is a game in which the player seeks to solve a puzzle by guessing 
+the letters of a secret word one at a time.
+
+Rules
+Jumper is played according to the following rules.
+
+The puzzle is a secret word randomly chosen from a list.
+The player guesses a letter in the puzzle.
+If the guess is correct, the letter is revealed.
+If the guess is incorrect, a line is cut on the player's parachute.
+If the puzzle is solved the game is over.
+If the player has no more parachute the game is over.
+
+
+Requirements
+The program must also meet the following requirements:
+
+The program must include a README file.
+The program must include class and method comments.
+The program must have at least four classes.
+The program must remain true to game play described in the overview.
+
+Have Some Fun
+Have some fun by enhancing the game any way you like. A few ideas are as 
+follows:
+
+Enhanced input validation.
+Enhanced game play and game over messages.
+Enhanced game display, e.g. parachute
+"""
