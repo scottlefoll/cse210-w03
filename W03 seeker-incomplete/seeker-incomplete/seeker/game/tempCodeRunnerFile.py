fcaa3cc1fa2adf914f42a27103d5aac6495dedc1
scottlefoll <51521131+scottlefoll@users.noreply.github.com>
2022-06-25 18:45:35 -0400

    update

---
+++ W03 seeker-incomplete/seeker-incomplete/seeker/game/tempCodeRunnerFile.py
@@ -0,0 +1,18 @@
+import random
+
+class Hider:
+    """The person hiding from the Seeker. 
+    
+    The responsibility of Hider is to keep track of its location and distance from the seeker. 
+    
+    Attributes:
+        _location (int): The location of the hider (1-1000).
+        _distance (List[int]): The distance from the seeker.
+    """
+
+    def __init__(self):
+        """Constructs a new Hider.
+
+        Args:
+            self (Hider): An instance of Hider.
+        """
